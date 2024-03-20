c1e554fceb1a50ad8abead21a0f26ed71978d202
Lee-hwansoo <lee_hwansoo@naver.com>
2024-03-21 02:37:36 +0900

    Added comment

--- src/path_planning/scripts/local_planner.py
+++ src/path_planning/scripts/local_planner.py
@@ -17,11 +17,11 @@ class path_pub :
         rospy.Subscriber("/global_path",Path, self.global_Path_callback)
 
         self.local_path_pub = rospy.Publisher('/local_path',Path, queue_size=1)
-
+        
         # 초기화
         self.global_path_msg=Path()
         self.global_path_msg.header.frame_id='/map'
-
+        
         self.is_status=False
         self.local_path_size=50
 
@@ -31,11 +31,11 @@ class path_pub :
 
         rate = rospy.Rate(20) # 20hz
         while not rospy.is_shutdown():
-
+   
             if self.is_status == True :
                 local_path_msg=Path()
                 local_path_msg.header.frame_id='/map'
-
+                
                 x=self.x
                 y=self.y
                 min_dis=float('inf')
@@ -47,7 +47,7 @@ class path_pub :
                         min_dis=distance
                         current_waypoint=i
 
-
+                
                 if current_waypoint != -1 :
                     if current_waypoint + self.local_path_size < len(self.global_path_msg.poses):
                         for num in range(current_waypoint,current_waypoint + self.local_path_size ) :
@@ -56,7 +56,7 @@ class path_pub :
                             tmp_pose.pose.position.y=self.global_path_msg.poses[num].pose.position.y
                             tmp_pose.pose.orientation.w=1
                             local_path_msg.poses.append(tmp_pose)
-
+                    
                     else :
                         for num in range(current_waypoint,len(self.global_path_msg.poses) ) :
                             tmp_pose=PoseStamped()
@@ -78,11 +78,10 @@ class path_pub :
 
 
     def global_Path_callback(self,msg):
-        self.global_path_msg = msg
+        self.global_path_msg = msg       
 
 if __name__ == '__main__':
     try:
         test_track=path_pub()
     except rospy.ROSInterruptException:
         pass
-
